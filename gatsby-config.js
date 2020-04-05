b9668f526f6323ccb6a88ab8cdc90e0f6ef4ce10
Saeed Asyn <63156600+s4asyn@users.noreply.github.com>
2020-04-06 01:49:37 +0430

    990118 => Add tailwindcss, postcss, purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,22 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-}
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-postcss',
+            options: {
+                postCssPlugins: [
+                    require(`tailwindcss`)(`./tailwind.config.js`),
+                    ...(process.env.NODE_ENV === 'production'
+                        ? [require(`autoprefixer`), require(`cssnano`)]
+                        : []),
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-purgecss`,
+            options: {
+                tailwind: true,
+                purgeOnly: [`src/css/style.css`],
+            },
+        },
+    ],
+};
