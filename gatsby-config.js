17d31c5d2b4a673f34b8b1a657a4777e51f0385d
Saeed Asyn <63156600+s4asyn@users.noreply.github.com>
2020-04-14 12:22:03 +0430

    990126 => Add google analitics

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,5 +52,32 @@ module.exports = {
                 tailwind: true,
             },
         },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+              // The property ID; the tracking code won't be generated without it
+              trackingId: "UA-163585154-1",
+              // Defines where to place the tracking script - `true` in the head and `false` in the body
+              head: false,
+              // Setting this parameter is optional
+              anonymize: true,
+              // Setting this parameter is also optional
+              respectDNT: true,
+              // Avoids sending pageview hits from custom paths
+              exclude: ["/do-not-track/me/too/"],
+              // Delays sending pageview hits on route update (in milliseconds)
+              pageTransitionDelay: 0,
+              // Enables Google Optimize using your container Id
+              optimizeId: "GTM-M5MD9VR",
+              // Enables Google Optimize Experiment ID
+              experimentId: " WuicPPzOR1yf5tSmXtMOvQ",
+              // Set Variation ID. 0 for original 1,2,3....
+              variationId: "0",
+              // Any additional optional fields
+              sampleRate: 5,
+              siteSpeedSampleRate: 10,
+              cookieDomain: "asaiyan.com",
+            },
+          },
     ],
 };
