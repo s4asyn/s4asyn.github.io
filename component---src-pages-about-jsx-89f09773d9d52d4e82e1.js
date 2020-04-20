df9bbe1afa65e3c4bd5982daade433465289fac8
Saeed Asyn <63156600+s4asyn@users.noreply.github.com>
2020-04-20 20:34:25 +0430

    9902012034

--- component---src-pages-about-jsx-89f09773d9d52d4e82e1.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{bT9l:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),c=n.n(a),i=n("Kvkj"),r={description:"سعید عصائیان",title:"درباره‌ی من"};t.default=function(){return c.a.createElement(i.a,{SeoData:r},c.a.createElement("article",null))}}}]);
-//# sourceMappingURL=component---src-pages-about-jsx-89f09773d9d52d4e82e1.js.map
