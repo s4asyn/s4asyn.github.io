496af4ec69bc3e49cbf87c8d1d4c0ec7d18e136c
Saeed Asyn <63156600+s4asyn@users.noreply.github.com>
2020-04-22 23:51:07 +0430

    990203 => Add click button to code block; change prismjs theme

--- src/components/markdown-post/markdown-post.jsx
+++ src/components/markdown-post/markdown-post.jsx
@@ -1,14 +1,38 @@
-import React, { Children } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import propTypes from 'prop-types';
+import Clipboard from 'clipboard';
 /* import Subcomponent */
 
 /* import Style*/
 import MarkDownStyle from './markdown-post.module.css';
 
 /* Start Component */
-const MarkDown = ({ children }) => (
-    <article className={MarkDownStyle.postBody}>{children}</article>
-);
+const MarkDown = ({ children }) => {
+    const createCopyButton = useCallback(() => {
+        const codes = global.document.querySelectorAll('.gatsby-highlight');
+
+        codes.forEach((code) => {
+            const button = document.createElement('button');
+            button.setAttribute('class', 'copy-button');
+            button.innerHTML = 'COPY';
+
+            code.appendChild(button);
+        });
+
+        const clipboard = new Clipboard('.copy-button', {
+            target: ({ previousElementSibling }) => previousElementSibling,
+        });
+
+        clipboard.on('success', (e) => {
+            e.clearSelection();
+        });
+    }, []);
+
+    useEffect(() => {
+        createCopyButton();
+    }, []);
+    return <article className={MarkDownStyle.postBody}>{children}</article>;
+};
 
 MarkDown.propTypes = {
     children: propTypes.node.isRequired,
