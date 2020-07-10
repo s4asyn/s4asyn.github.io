447482137d54c8f8ffd8bdb24c361c24040a0c5c
Saeed Asyn <s4asyn@users.noreply.github.com>
2020-07-10 13:01:57 +0430

    990420 => Redesign: (Step 7)
    - redesign landing page
    -  fix some border warning in markdown css
    - add style for em and strong in markdown css

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 
-import { Layout, BlogCard } from '#components';
-
-const SeoData = {
-    description: 'فهرست مطالب وبلاگ',
-    title: 'سعید عصائیان',
-};
+import { Layout, SocialLinks } from '#components';
+import MainStyle from '#components/main/main.module.css';
 
 export default ({ data }) => {
+    const SeoData = {
+        description: 'صفحه‌ی خانگی',
+        title: 'سعید عصائیان',
+    };
     return (
-        <Layout SeoData={SeoData}>
-            <article>
-                <section>
-                    {/* <h4>{data.allMdx.totalCount} Posts</h4> */}
-                    {data.allMdx.edges.map(({ node }) => (
-                        <BlogCard key={node.id} BlogNode={node} />
-                    ))}
-                </section>
-            </article>
+        <Layout SeoData={SeoData} justSeo={true}>
+            <div className={MainStyle.MainStyle}>
+                <header className="flex flex-col justify-center w-full h-screen border border-black">
+                    <h1 className="text-4xl font-bold text-center text-primary">
+                        <Link to="/about">{SeoData.title}</Link>
+                    </h1>
+                    <SocialLinks />
+                    <ul className="flex flex-wrap mt-16 text-lg font-semibold text-primary">
+                        <li className="w-1/2 text-center">
+                            <Link to="/fa/blog">نوشته‌ها</Link>(
+                            {data.blog.totalCount})
+                        </li>
+                        <li className="w-1/2 text-center">
+                            <Link to="/fa/library">کتاب‌خانه</Link>(
+                            {data.library.totalCount})
+                        </li>
+                    </ul>
+                </header>
+            </div>
         </Layout>
     );
 };
 
 export const query = graphql`
     query {
-        allMdx(
+        blog: allMdx(
             filter: {
                 frontmatter: { published: { eq: "yes" }, type: { eq: "blog" } }
             }
@@ -34,7 +44,24 @@ export const query = graphql`
             totalCount
             edges {
                 node {
-                    ...BlogNode
+                    fields {
+                        ...Tags
+                    }
+                }
+            }
+        }
+        library: allMdx(
+            filter: {
+                frontmatter: { published: { eq: "yes" }, type: { eq: "book" } }
+            }
+            sort: { fields: frontmatter___date, order: DESC }
+        ) {
+            totalCount
+            edges {
+                node {
+                    fields {
+                        ...Tags
+                    }
                 }
             }
         }
