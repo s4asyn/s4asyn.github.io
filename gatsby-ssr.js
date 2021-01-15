70fd4802607bd3c2a5e8f2e6c50c5f2f1ae8facd
Saeed Asyn <s4asyn@users.noreply.github.com>
2021-01-16 01:05:23 +0330

    13991027 => Fix FOUC problem

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,6 +3,22 @@ import './src/styles/base.css';
 import React from 'react';
 import { WrapGatsbyPage } from '#components';
 
+const HtmlAttributes = {
+    lang: "fa",
+    dir: "rtl"
+}
+
+const BodyAttributes = {
+    className: "body-theme-light bg-body body-lang-farsi font-body"
+}
+
+export const onRenderBody = ({
+    setHtmlAttributes,
+    setBodyAttributes
+}, pluginOptions) => {
+    setHtmlAttributes(HtmlAttributes)
+    setBodyAttributes(BodyAttributes)
+}
 export const wrapPageElement = ({ element, props }) => (
     <WrapGatsbyPage {...props}>{element}</WrapGatsbyPage>
 );
